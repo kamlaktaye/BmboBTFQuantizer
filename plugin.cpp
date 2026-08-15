80f5a69ea9ee1f00fa823f33d2326b9ccc44eb2f
Kamlak Taye <kamlakbmbo@gmail.com>
2026-08-15 14:40:52 -0700

    Update model added in plugin initialization

--- plugin.cpp
+++ plugin.cpp
@@ -6,7 +6,7 @@ void init(Plugin* p) {
 	pluginInstance = p;
 
 	// Add modules here
-	p->addModel(modelBmboBTFQuantizer);
+	p->addModel(modelBTFQuantizer);
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when
